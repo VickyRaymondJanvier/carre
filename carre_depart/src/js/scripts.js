1a9d89c8d2795c46bf9cb410055f7aa35b631d02
VickyRaymondJanvier <2285940@cmontmorency.qc.ca>
2025-10-06 12:33:18 -0400

    vrai_remise_final

---
+++ carre_depart/src/js/scripts.js
@@ -0,0 +1,9 @@
+
+//A finir
+function img() {
+    document.getElementById("active").classList.add('MyClass');
+ }
+ function on_click img() {
+ 
+    document.getElementById("disable").classList.add('MyClass');
+ }
